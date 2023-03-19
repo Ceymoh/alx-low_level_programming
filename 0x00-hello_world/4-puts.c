8a63189944b6ae922e6a2274500c68aeb884738b
Ceymoh <muthiijames@gmail.com>
2023-03-19 06:07:07 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	/*description of programming language*/
 
-	puts("\"programming is like building a mulitlingual puzzle");
+	puts("\"programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
