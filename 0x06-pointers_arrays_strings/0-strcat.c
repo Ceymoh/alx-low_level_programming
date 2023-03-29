70e8b3529ca751473ae162ece64ddf933dd0fff0
Ceymoh <muthiijames@gmail.com>
2023-03-29 07:45:19 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the values used for input
+ * @src: the values used for input
+ * Return: success
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\n';
+	return (dest);
+
+}
+
+
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+concatenates two strings using _strcat
