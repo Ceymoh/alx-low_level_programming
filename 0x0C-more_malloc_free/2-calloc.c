a2eb61b29a7e022e833d93313c94b2b8cdb07fad
Ceymoh <muthiijames@gmail.com>
2023-04-13 09:58:06 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,9 +17,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *s;
 
 	j = nmemb * size;
-
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0 && size == 0)
 		return (NULL);
+
 	s = malloc(sizeof(j));
 
 	if (s == NULL)
